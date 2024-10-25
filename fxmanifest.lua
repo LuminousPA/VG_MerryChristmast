f0e4c357b185e0cbb5d72b67519c758b673d9550
LuminousPA <luminouspineapple88@gmail.com>
2024-10-25 20:33:21 +0700

    add

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+game 'gta5'
+fx_version 'cerulean'
+
+name 'VG | Merry Christmast'
+author 'Vinh Gold'
+lua54 'yes'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_script {
+    'main.lua'
+}
+
+server_script {
+    'main.lua'
+}
